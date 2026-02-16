f3de52ebcb682f31812434e25543355e290820e5
haafidhahabs <haafidhah.abs@gmail.com>
2026-02-16 12:08:48 +0800

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+{
+  "name": "e-PDP SMK Semenchu",
+  "short_name": "e-PDP",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#d9f4e0",
+  "theme_color": "#1b5e20",
+  "orientation": "portrait",
+  "icons": []
+}
